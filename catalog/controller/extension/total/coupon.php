dec888b34b84f49a5ea72c87d9530228d5be70ba
Denver777 <den.yunakov@gmail.com>
2017-11-27 01:59:00 +0200

    Task finished

---
+++ catalog/controller/extension/total/coupon.php
@@ -0,0 +1,68 @@
+<?php
+class ControllerExtensionTotalCoupon extends Controller {
+	public function index() {
+		if ($this->config->get('coupon_status')) {
+			$this->load->language('extension/total/coupon');
+
+			$data['heading_title'] = $this->language->get('heading_title');
+
+			$data['text_loading'] = $this->language->get('text_loading');
+
+			$data['entry_coupon'] = $this->language->get('entry_coupon');
+
+			$data['button_coupon'] = $this->language->get('button_coupon');
+
+			if (isset($this->session->data['coupon'])) {
+				$data['coupon'] = $this->session->data['coupon'];
+			} else {
+				$data['coupon'] = '';
+			}
+
+			return $this->load->view('extension/total/coupon', $data);
+		}
+	}
+
+	public function coupon() {
+		$this->load->language('extension/total/coupon');
+
+		$json = array();
+
+		$this->load->model('extension/total/coupon');
+
+		if (isset($this->request->post['coupon'])) {
+			$coupon = $this->request->post['coupon'];
+		} else {
+			$coupon = '';
+		}
+
+		$coupon_info = $this->model_extension_total_coupon->getCoupon($coupon);
+
+        if($this->session->data['currency'] == 'UAH' && $this->currency->getValue($this->session->data['currency'])*$this->cart->getSubTotal() < 1000 && $this->request->post['coupon'] == 'over1000purchase') {
+            $coupon_info = null;
+            $coupon = '';
+        }
+
+		if (empty($this->request->post['coupon'])) {
+			$json['error'] = $this->language->get('error_empty');
+
+			unset($this->session->data['coupon']);
+		} elseif ($coupon_info) {
+			$this->session->data['coupon'] = $this->request->post['coupon'];
+
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$json['redirect'] = $this->url->link('checkout/cart');
+		} else {
+			$json['error'] = $this->language->get('error_coupon');
+		}
+
+        if($this->session->data['currency'] == 'UAH' && $this->currency->getValue($this->session->data['currency'])*$this->cart->getSubTotal() < 1000 && $this->request->post['coupon'] == 'over1000purchase') {
+            unset($this->session->data['coupon']);
+            $json['error'] = $this->language->get('error_coupon');
+            $json['redirect'] = $this->url->link('checkout/cart');
+        }
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+}
